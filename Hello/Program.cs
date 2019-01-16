d83c6e6fbf02b424cc4c6ca61f0c9a6e51da68e6
DevDuck33 <game.dev@op.pl>
2019-01-16 22:05:23 +0100

    Wyodrębnienie metod programu.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,15 +6,29 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string Imie;
-            int Wiek;
-            Console.Write("Wpisz swoje imie: ");
-            Imie = Console.ReadLine();
-            Console.WriteLine("Witaj " + Imie);
+            for (; ; )
+            {
+                PodajImie();
+                PodajWiek();
+                ResetKonsoli();
+            }
+        }
 
+        private static void ResetKonsoli()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        private static void PodajWiek()
+        {
+            int Wiek;
             Console.Write("Podaj swój wiek: ");
-            bool wynik= int.TryParse(Console.ReadLine(),out Wiek);       //Parse to konwersja string na int
-                                                                         //TryParse konwersja która zwraca true jeżeli przebiegła pomyślnie przekazując wiek do "Out Wiek"
+            bool wynik = int.TryParse(Console.ReadLine(), out Wiek);
+                    //Parse to konwersja string na int
+                    //TryParse konwersja która zwraca true jeżeli przebiegła pomyślnie przekazując wiek do "Out Wiek"
+
             if (wynik == false)
             {
                 Console.WriteLine("Wpisałeś coś innego niż wiek !");
@@ -23,15 +37,23 @@ namespace Hello
             {
                 if (Wiek < 18)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Możesz kupić wodę :(");
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Możesz kupić alkohol :)");
                 }
             }
+        }
 
-            Console.ReadKey();
+        private static void PodajImie()
+        {
+            string Imie;
+            Console.Write("Wpisz swoje imie: ");
+            Imie = Console.ReadLine();
+            Console.WriteLine("Witaj " + Imie);
         }
     }
 }
