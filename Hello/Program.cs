511e0e1f0b24df2450e64abcf62843057ad43608
DevDuck33 <game.dev@op.pl>
2019-01-16 21:49:55 +0100

    Prosty program imitujący sprzedaż alkoholu w barze.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,37 @@
-﻿
+﻿using System;
+
 namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string Text;
+            string Imie;
+            int Wiek;
+            Console.Write("Wpisz swoje imie: ");
+            Imie = Console.ReadLine();
+            Console.WriteLine("Witaj " + Imie);
 
-            System.Console.WriteLine("Hello World");
-            Text = System.Console.ReadLine();
-            System.Console.WriteLine(Text);
+            Console.Write("Podaj swój wiek: ");
+            bool wynik= int.TryParse(Console.ReadLine(),out Wiek);       //Parse to konwersja string na int
+                                                                         //TryParse konwersja która zwraca true jeżeli przebiegła pomyślnie przekazując wiek do "Out Wiek"
+            if (wynik == false)
+            {
+                Console.WriteLine("Wpisałeś coś innego niż wiek !");
+            }
+            else
+            {
+                if (Wiek < 18)
+                {
+                    Console.WriteLine("Możesz kupić wodę :(");
+                }
+                else
+                {
+                    Console.WriteLine("Możesz kupić alkohol :)");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
