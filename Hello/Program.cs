2bf8ff0f623a8cbe1d2e08cdbb2ce69c9d70364c
DevDuck33 <game.dev@op.pl>
2019-01-16 21:12:24 +0100

    First program in clear C# <Hello World>

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string Text;
+
+            System.Console.WriteLine("Hello World");
+            Text = System.Console.ReadLine();
+            System.Console.WriteLine(Text);
         }
     }
 }
