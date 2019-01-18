33eeca78d53910a6a369fed766473613a55e2e16
DevDuck33 <game.dev@op.pl>
2019-01-18 23:06:11 +0100

    Tworzenie osób za pomocą własnego konstruktora.

---
+++ Constructors/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Person
+    {
+        int Id;
+        string FirstName;
+        string LastName;
+        string Address;
+        int Age;
+        string Gender;
+
+
+        public Person(int Id, string FirstName, string LastName, string Address, int Age, string Gender)        //konstruktor osoby 
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.Age = Age;
+            this.Gender = Gender;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person = new Person(1, "Adam", "Nowak", "Warsaw", 21, "Male");       //inicjalizacja nowej osoby za pomocą naszego konstruktora
+        }
+    }
+}
